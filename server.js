46cbbc1f74cd10105776a08e8172693336113235
Chiza Barber <chiza.alba@gmail.com>
2019-01-10 20:02:55 -0600

    initialized knex

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const knexLib = require('knex')
+const fs = require('fs-plus')
 const app = express()
 
 app.use(express.static('Public'))
@@ -6,3 +8,20 @@ app.use(express.static('Public'))
 app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port 3000.')
 })
+
+
+// const defaultDbOptions = {
+//     client: 'sqlite3',
+//     connection: {
+//         filename : 'database.db'
+//     }
+// }
+
+// let dbOptions = defaultDbOptions
+// try {
+//     dbOptions = JSON.parse(fs.readFileSync('./config.json'))
+// } catch (e) {}
+
+// console.log('the dbOptions:', dbOptions)
+
+// var knex = knexLib(dbOptions)
