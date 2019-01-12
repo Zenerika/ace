172ef5f8b6efa39eeea6c7a6b18673ccb60085a9
Addison Verger <addison.j.verger@gmail.com>
2019-01-12 13:46:24 -0600

    updates to website form

---
+++ Public/app.js
@@ -0,0 +1,5 @@
+/* AJAX Dynamic Drop-Down */
+
+//var url = // need to pull column from db //
+
+//var widget = new AutoComplete('breedInput', config)
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/* Express */
+
 const express = require('express')
 const knexLib = require('knex')
 const fs = require('fs-plus')
@@ -5,9 +7,84 @@ const app = express()
 
 app.use(express.static('Public'))
 
+app.get('/', function (req, res) {
+  res.sendFile('login.html', { root : __dirname })
+})
+
+/* Express Validator */
+
+const { body,validationResult } = require('express-validator/check')
+const { sanitizeBody } = require('express-validator/filter')
+
+/* Passport Setup */
+
+const passport = require('passport')
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/success', function (req, res) {
+  res.send("You have successfully logged in")
+})
+app.get('/error', function (req, res) {
+  res.send("Error logging in")
+})
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user.id)
+})
+passport.deserializeUser(function (id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err) }
+    cb(null, user)
+  })
+})
+
+/* Local Auth */
+
+// figure out db (database user id searching) //
+
+const Strategy = require('passport-local').Strategy
+
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err) }
+      if (!user) { return cb(null, false) }
+      if (user.password != password) { return cb(null, false) }
+      return cb(null, user)
+    })
+  }))
+
+/* Facebook Auth */
+
+const FacebookStrategy = require('passport-facebook').Strategy
+
+const FACEBOOK_APP_ID = '1972214219741500'
+const FACEBOOK_APP_SECRET = '52caefe50fa829ae902d8c69c60617dd'
+
+passport.use(new FacebookStrategy({
+  clientID: FACEBOOK_APP_ID,
+  clientSecret: FACEBOOK_APP_SECRET,
+  callbackURL: "/auth/facebook/callback"
+},
+function (accessToken, refreshToken, profile, cb) {
+  return cb(null, profile)
+}
+))
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {failureRedirect: '/error'}),
+  function(req, res) {
+    res.redirect('/success')
+})
+
 app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port 3000.')
 })
+<<<<<<< HEAD
 
 
 // const defaultDbOptions = {
@@ -25,3 +102,5 @@ app.listen(process.env.PORT || 3000, () => {
 // console.log('the dbOptions:', dbOptions)
 
 // var knex = knexLib(dbOptions)
+=======
+>>>>>>> updates to website form
